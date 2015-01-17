06563a9193531f18bea19bebd228942dec6913f2
joesitton <sittonjoe@gmail.com>
2015-01-17 16:18:41 -0500

    Update PWM and joystick channels

--- robot.py
+++ robot.py
@@ -8,34 +8,29 @@ class Robot(wpilib.IterativeRobot):
 
         # Motors to PWM channels
         fl_motor = wpilib.Talon(0) # Channel 0
-        bl_motor = wpilib.Talon(1)
-        fr_motor = wpilib.Talon(2)
-        br_motor = wpilib.Talon(3)
+        bl_motor = wpilib.Talon(1) # Channel 1
 
         # Drivetrain control
-        self.robot_drive = wpilib.RobotDrive(fl_motor, bl_motor, fr_motor, br_motor)
+        self.robot_drive = wpilib.RobotDrive(bl_motor, fl_motor)
 
         # Joystick
-        self.joystick = wpilib.Joystick(1)
+        self.joystick = wpilib.Joystick(0)
 
     def disabledInit(self):
-        pass
+        print("-- DISABLED --")
 
     def disabledPeriodic(self):
-        self.robot_drive.drive(0, 0)
+        self.robot_drive.drive(0, 0) # Stop the robot
 
     def autonomousInit(self):
+        print("-- AUTONOMOUS --")
         self.counter = 0
 
     def autonomousPeriodic(self):
-        if self.counter < 100:
-            self.robot_drive.drive(-0.5, 0)
-            self.counter +=1
-        else:
-            self.robot_drive.drive(0, 0)
+        pass
 
     def teleopInit(self):
-        pass
+        print("-- TELEOP --")
 
     def teleopPeriodic(self):
         self.robot_drive.arcadeDrive(self.joystick)
