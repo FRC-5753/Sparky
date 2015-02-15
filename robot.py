736a464c09047619f0462c164be92aa508fe483e
joesitton <sittonjoe@gmail.com>
2015-02-15 00:33:52 -0500

    Add limit switches, 2nd joystick

--- robot.py
+++ robot.py
@@ -10,13 +10,21 @@ class Sparky(w.IterativeRobot):
         # Motors to PWM channels
         l_motor = w.Talon(0)
         r_motor = w.Talon(1)
-        self.lift_motor = w.PWM(2)
+        self.lift_motor = w.Talon(2)
+
+        # Limit switches
+        self.top_ls = w.DigitalInput(0)
+        self.bot_ls = w.DigitalInput(1)
 
         # Drivetrain control
         self.drivetrain = w.RobotDrive(l_motor, r_motor)
 
         # Joystick
-        self.joystick = w.Joystick(0)
+        self.js0 = w.Joystick(0) # Xbox controller
+        self.js1 = w.Joystick(1) # Joystick
+
+        # Disable motor safety
+        self.drivetrain.setSafetyEnabled(False)
 
     def disabledInit(self):
         pass
@@ -28,24 +36,23 @@ class Sparky(w.IterativeRobot):
         self.move = self.drivetrain.drive
         self.moving = True
         self.i = 0
-        self.cap = 100
-        self.max_speed = 10
+
+        self.length = 100 # How long the robot will run at max speed (100 ~= 2 sec)
+        self.max_speed = 5 # Maximum speed (1-10)
 
     def autonomousPeriodic(self):
         while self.moving:
             for x in range(self.max_speed):
-                print(x)
                 self.move(-x/10, -0.08)
                 sleep(0.1)
                 if x == (self.max_speed - 1):
                     self.moving = False
 
-        if self.i < self.cap:
+        if self.i < self.length:
             self.move(-self.max_speed/10, -0.08)
             self.i += 1
-        elif self.i == self.cap:
+        elif self.i == self.length:
             for x in reversed(range(self.max_speed - 1)):
-                print(x)
                 self.move(-x/10, 0)
                 sleep(0.1)
                 if x == 1:
@@ -55,20 +62,27 @@ class Sparky(w.IterativeRobot):
             self.move(0, 0)
 
     def teleopInit(self):
-        w.kDefaultSensitivity = 0.25
+        self.up = True
+        self.down = True
 
     def teleopPeriodic(self):
         while self.isOperatorControl() and self.isEnabled():
-            self.drivetrain.arcadeDrive(self.joystick)
+            self.drivetrain.arcadeDrive(self.js0, squaredInputs=True)
 
-            if self.joystick.getRawButton(1):
-                self.lift_motor.setBounds(10, 10, 5, 1, 1)
-                self.lift_motor.setSpeed(1)
-            elif self.joystick.getRawButton(2):
-                self.lift_motor.setBounds(10, 10, 5, 1, 1)
-                self.lift_motor.setSpeed(-1)
+            if self.top_ls.get() == 1:
+                self.up = False
+            elif self.bot_ls.get() == 1:
+                self.down = False
             else:
-                self.lift_motor.setBounds(0, 0, 0, 0, 0)
+                self.up = True
+                self.down = True
+
+            if self.js0.getRawButton(1) and self.up:
+                self.lift_motor.set(1)
+            elif self.js0.getRawButton(2) and self.down:
+                self.lift_motor.set(-1)
+            else:
+                self.lift_motor.set(0)
 
 if __name__ == "__main__":
     w.run(Sparky)
