f928ce13100cb3ed39d9cb52b2835bea17aa2ae4
joesitton <sittonjoe@gmail.com>
2015-02-14 22:21:22 -0500

    Cleanup, add autonomous and temporary teleop lift controls

--- robot.py
+++ robot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import wpilib as w
+from time import sleep
 
 class Sparky(w.IterativeRobot):
 
@@ -9,36 +10,65 @@ class Sparky(w.IterativeRobot):
         # Motors to PWM channels
         l_motor = w.Talon(0)
         r_motor = w.Talon(1)
+        self.lift_motor = w.PWM(2)
 
         # Drivetrain control
-        self.drivetrain = w.RobotDrive(r_motor, l_motor)
+        self.drivetrain = w.RobotDrive(l_motor, r_motor)
 
         # Joystick
         self.joystick = w.Joystick(0)
-
-    def move(self, speed, curve):
-        self.drivetrain.drive(speed, curve)
-
-    def stop(self):
-        self.drivetrain.drive(0, 0)
 
     def disabledInit(self):
         pass
 
     def disabledPeriodic(self):
-        self.stop() # Stop the robot
+        self.drivetrain.drive(0, 0)
 
     def autonomousInit(self):
-        pass
+        self.move = self.drivetrain.drive
+        self.moving = True
+        self.i = 0
+        self.cap = 100
+        self.max_speed = 10
 
     def autonomousPeriodic(self):
-        pass
+        while self.moving:
+            for x in range(self.max_speed):
+                print(x)
+                self.move(-x/10, -0.08)
+                sleep(0.1)
+                if x == (self.max_speed - 1):
+                    self.moving = False
+
+        if self.i < self.cap:
+            self.move(-self.max_speed/10, -0.08)
+            self.i += 1
+        elif self.i == self.cap:
+            for x in reversed(range(self.max_speed - 1)):
+                print(x)
+                self.move(-x/10, 0)
+                sleep(0.1)
+                if x == 1:
+                    self.move(0, 0)
+                    self.i += 1
+        else:
+            self.move(0, 0)
 
     def teleopInit(self):
-        pass
+        w.kDefaultSensitivity = 0.25
 
     def teleopPeriodic(self):
-        pass
+        while self.isOperatorControl() and self.isEnabled():
+            self.drivetrain.arcadeDrive(self.joystick)
+
+            if self.joystick.getRawButton(1):
+                self.lift_motor.setBounds(10, 10, 5, 1, 1)
+                self.lift_motor.setSpeed(1)
+            elif self.joystick.getRawButton(2):
+                self.lift_motor.setBounds(10, 10, 5, 1, 1)
+                self.lift_motor.setSpeed(-1)
+            else:
+                self.lift_motor.setBounds(0, 0, 0, 0, 0)
 
 if __name__ == "__main__":
     w.run(Sparky)
