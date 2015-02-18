70675c3f311093d7894501ec7818efcde7c1a818
joesitton <sittonjoe@gmail.com>
2015-02-18 08:14:11 -0500

    Reduce auton time

--- robot.py
+++ robot.py
@@ -30,19 +30,19 @@ class Sparky(w.IterativeRobot):
         self.moving = True
         self.i = 0
 
-        self.length = 145 # How long the robot will run at max speed (100 ~= 2 sec)
+        self.length = 130 # How long the robot will run at max speed (100 ~= 2 sec)
         self.max_speed = 5 # Maximum speed (1-10)
 
     def autonomousPeriodic(self):
         while self.moving:
             for x in range(self.max_speed):
-                self.move(-x/10, -0.07)
+                self.move(-x/10, 0)
                 sleep(0.1)
                 if x == (self.max_speed - 1):
                     self.moving = False
 
         if self.i < self.length:
-            self.move(-self.max_speed/10, -0.07)
+            self.move(-self.max_speed/10, 0)
             self.i += 1
         elif self.i == self.length:
             for x in reversed(range(self.max_speed - 1)):
